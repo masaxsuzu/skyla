e4b52d8d07cdd2e2ad559142f7145a547348ce02
masaxsuzu <masaxsuzu+github@gmail.com>
2022-01-03 11:23:12 +0900

    Handle cancellation

--- demo/Program.cs
+++ demo/Program.cs
@@ -22,28 +22,37 @@ var tx = s.Create();
 var driver = new NaiveDriver(s, tx);
 Console.WriteLine("Hello, Skyla!");
 
-/*
+#pragma warning disable CS8622
+Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs args) {
+    args.Cancel = true;
+    Console.WriteLine("terminating...");
+    System.Environment.Exit(-1);
+};
 
-create table a (x int, y varchar(2), z varchar(10))
-create view v1 as select x,y,z from a where x = 1
-create view v2 as select x,y,z from a where y = '2'
-create view v3 as select x,y,z from a where z = '3'
-insert into a (x,y,z) values (0, '0', '0')
-insert into a (x,y,z) values (0, '0', '3')
-insert into a (x,y,z) values (0, 'あ', '0')
-insert into a (x,y,z) values (0, '2', '3')
-insert into a (x,y,z) values (1, '0', '0')
-insert into a (x,y,z) values (1, '0', '3')
-insert into a (x,y,z) values (1, '2', '0')
-insert into a (x,y,z) values (1, '2', '日本語')
+var samples = new string[]{
+    "create table a (x int, y varchar(2), z varchar(10))",
+    "create view v1 as select x,y,z from a where x = 1",
+    "create view v2 as select x,y,z from a where y = '2'",
+    "create view v3 as select x,y,z from a where z = '3'",
+    "insert into a (x,y,z) values (0, '0', '0')",
+    "insert into a (x,y,z) values (0, '0', '3')",
+    "insert into a (x,y,z) values (0, 'あ', '0')",
+    "insert into a (x,y,z) values (0, '2', '3')",
+    "insert into a (x,y,z) values (1, '0', '0')",
+    "insert into a (x,y,z) values (1, '0', '3')",
+    "insert into a (x,y,z) values (1, '2', '0')",
+    "insert into a (x,y,z) values (1, '2', '日本語')",
+    "select x,y,z from a",
+    "select x,y,z from a where x = 1 and y = '2'",
+    "select x,y,z from v1",
+    "select x,y,z from v2",
+    "select x,y,z from v3",
+};
 
-select x,y,z from a
-select x,y,z from a where x = 1 and y = '2'
-select x,y,z from v1
-select x,y,z from v2
-select x,y,z from v3
-
-*/
+foreach (var sample in samples)
+{
+    var ret = driver.Drive(sample);
+}
 
 while (true)
 {
@@ -52,5 +61,5 @@ while (true)
     if(sql == ":q")
     break;
     var ret = driver.Drive(sql);
-    Console.WriteLine(ret.Message);
+    Console.WriteLine($"{ret.Message}");
 }
